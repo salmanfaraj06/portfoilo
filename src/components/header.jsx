0d1b6cfc8038501d2330775517e472b989d32839
salmanfaraj06 <salman.faraj06@gmail.com>
2024-03-25 13:35:40 +0530

    Fix file name case in header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -73,7 +73,7 @@ function Header() {
         </div>
         <div className="flex items-center space-x-5">
           <a
-            href="/assets/salman-cv.pdf"
+            href="/assets/Salman-cv.pdf"
             download
             onClick={handleDownload}
             
