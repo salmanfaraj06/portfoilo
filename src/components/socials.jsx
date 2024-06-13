9aed6adddb792a349890fa1155d9ca1a60420f73
salmanfaraj06 <salman.faraj06@gmail.com>
2024-06-13 23:34:51 +0530

    Refactor socials component to use icons instead of images

--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -1,35 +1,47 @@
 import React from "react";
 import Img from "./img";
+import { FaInstagram, FaGithub, FaLinkedin, FaMedium } from "react-icons/fa";
 
 function Socials() {
   const socials = [
     {
       href: "https://www.instagram.com/salmaaan.fr/",
-      imgSrc: "images/img_socialicon_black_900.svg",
-      alt: "Instagram icon"
+      icon: <FaInstagram />,
+      alt: "Instagram icon",
     },
     {
       href: "https://github.com/salmanfaraj06",
-      imgSrc: "images/git.svg",
-      alt: "GitHub icon"
+      icon: <FaGithub />,
+      alt: "GitHub icon",
     },
     {
       href: "https://www.linkedin.com/in/salman-faraj-2617a924b/",
-      imgSrc: "images/img_iconlinkedin.svg",
-      alt: "LinkedIn icon"
-    }
+      icon: <FaLinkedin />,
+      alt: "LinkedIn icon",
+    },
+    {
+      href: "https://medium.com/@salman.faraj06",
+      icon: <FaMedium />,
+      alt: "Medium icon",
+    },
   ];
 
   return (
     <div className="mx-auto max-w-6xl md:max-w-[600px]">
-    <div className="flex sm:justify-center md:justify-start sm:ml-0 gap-x-9 sm:gap-x-5 sm:mt-5">
-      {socials.map((social, index) => (
-        <a key={index} href={social.href} target="_blank" rel="noopener noreferrer">
-          <Img className="h-8 w-8 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" src={social.imgSrc} alt={social.alt} />
-        </a>
-      ))}
-    </div>
+  <div className="flex sm:justify-center md:justify-start sm:ml-0 gap-x-9 sm:gap-x-5 sm:mt-5">
+    {socials.map((social, index) => (
+      <a
+        key={index}
+        href={social.href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+      >
+        {React.cloneElement(social.icon, { size: "2em" })}
+      </a>
+    ))}
   </div>
+</div>
   );
 }
 
